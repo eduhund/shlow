a5e2eac4d64fd19bac91a4e5aaf27bd01580d199
Roman Nebel <im.roman.nebel@gmail.com>
2024-10-15 21:01:53 +0200

    Fix UI reset after the initial connector paste

--- src/plugin/connector.ts
+++ src/plugin/connector.ts
@@ -82,6 +82,9 @@ export const { checkInitConnector, setInitConnector, createConnector } = (() => 
           arrow.locked = true;
           arrow.name = '_flow-init-connector';
           setInitConnector(arrow);
+          figma.ui.postMessage({
+            type: 'SET_READY',
+          });
         }
       });
       return false;
@@ -89,16 +92,6 @@ export const { checkInitConnector, setInitConnector, createConnector } = (() => 
 
     return true;
   }
-
-  //function getInitConnector() {
-  //  return (
-  //    initConnector ||
-  //    figma.ui.postMessage({
-  //      type: 'GET_INIT_CONNECTOR',
-  //      data: { connectorTemplate },
-  //    })
-  //  );
-  //}
 
   function setInitConnector(node: ConnectorNode) {
     initConnector = node;
